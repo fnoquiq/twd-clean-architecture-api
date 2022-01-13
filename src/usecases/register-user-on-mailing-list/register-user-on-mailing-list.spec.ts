3f3fb6985384e7ad59d27bf6e12cde2d973f7ed6
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-01-13 18:00:05 -0300

    feat: user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,7 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+  })
+})
