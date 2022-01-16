b39a84ae669d5f94a47ff45c6488861973d5a356
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-01-16 13:34:14 -0300

    feat: validate email with domain part with invalid char

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,10 +1,16 @@
 export class Email {
   static validate(email: string) {
+    
     if (!email) {
       return false
     }
 
     if (email.length > 320) {
+      return false
+    }
+
+    const email_regex = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+    if (!email_regex.test(email)) {
       return false
     }
 
