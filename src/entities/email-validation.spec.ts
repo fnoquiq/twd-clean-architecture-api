6deb7a0bd7fc15d2e8adff646732146353c3dd49
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-01-16 12:34:45 -0300

    test: validate emails with domain larger than 255 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -21,6 +21,11 @@ describe('Email Validator', () => {
     expect(Email.validate(email)).toBeFalsy()
   })
 
+  test('should not accept domain part larger than 255 char', () => {
+    const email = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
   test('should not accept local part larger than 64 char', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
