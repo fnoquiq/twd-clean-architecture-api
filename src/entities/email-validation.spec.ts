a374310009d552a3d10a6c079d5038dd583d88a3
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-01-16 11:25:49 -0300

    test: validate local part larger than 64 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -1,18 +1,23 @@
 import { Email } from './email'
 
 describe('Email Validator', () => {
-  test('Should not accept null strings', () => {
+  test('should not accept null strings', () => {
     const email = null
     expect(Email.validate(email)).toBeFalsy()
   })
 
-  test('Should not accept empty strings', () => {
+  test('should not accept empty strings', () => {
     const email = ""
     expect(Email.validate(email)).toBeFalsy()
   })
 
-  test('Should accept valid email', () => {
+  test('should accept valid email', () => {
     const email = 'any@email.com'
     expect(Email.validate(email)).toBeTruthy()
+  })
+
+  test('should not accept local part larger than 64 char', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
   })
 })
